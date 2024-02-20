f06036503984809063b0f22c255f87b649ebd500
shuding0307 <suhyun2116@gmail.com>
2024-02-20 09:04:23 +0900

    day4-webtoon-swiper

---
+++ components/Header/WebtoonSwiper.tsx
@@ -0,0 +1,39 @@
+import Swiper from 'react-native-swiper';
+import { Dimensions } from 'react-native';
+import { Box } from '@gluestack-ui/themed';
+import { useQuery } from '@tanstack/react-query';
+import { Webtoon } from '../../types';
+import LargeCard from '../Card/LargeCard';
+
+interface WebtoonResponse {
+    webtoons: Webtoon[];
+}
+
+const fetchWebtoons = async () => {
+    const res = await fetch('https://korea-webtoon-api.herokuapp.com');
+    return res.json();
+};
+
+const WebToonSwiper = () => {
+    const {data} = useQuery<WebtoonResponse>({
+        queryKey: ['repoData'],
+        queryFn: fetchWebtoons,
+    });
+
+    const swiperWebtoons = data?.webtoons.slice(0,5) || [];
+
+    return (
+        <Box justifyContent='center' alignItems='center'>
+                {swiperWebtoons && swiperWebtoons?.length > 0 && (
+                <Swiper showsButtons={false} dotColor='#BDBDBD'>
+                    {swiperWebtoons.map((webtoon) => (
+                        <LargeCard key={webtoon.webtoonId} webtoon={webtoon} />
+                    ))}
+                </Swiper>
+                )}
+        </Box>
+    );
+
+};
+
+export default WebToonSwiper;
---
+++ types.ts
@@ -0,0 +1,6 @@
+export interface Webtoon {
+    webtoonId: string;
+    title: string;
+    img: string;
+    author: string;
+  }
